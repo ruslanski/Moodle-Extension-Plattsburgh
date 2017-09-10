4d602af91abea13e5e64c5b94a5b032a3d039a45
Jayant Arora <jaistudy1996@gmail.com>
2017-09-09 22:38:46 -0400

    Add documentation and implement user login using promises

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,20 +1,43 @@
 /* jshint esversion:6 */
 
+/**
+ * @author Jayant Arora
+ */
+
+/**
+ * @function makeXHRreq [Make XHR requests]
+ * @param  {string} url          [The url that for which xhr needs to be called]
+ * @param  {string} method       [method of xhr to be called.]
+ * @param  {string} responseType [Type of response expected from xhr]
+ * @return {Promise}              [A Promise is retured for the called XHR]
+ */
 function makeXHRreq(url, method, responseType){
   return new Promise(function(resolve, reject){
     let xhr = new XMLHttpRequest();
     xhr.responseType = responseType;
-    xhr.onload = function(){resolve(xhr.response);};
+    xhr.onload = function(){resolve(xhr);};
     xhr.onerror = function(){reject(xhr.statusText);};
     xhr.open(method, url, true);
     xhr.send();
   });
 }
 
+/**
+ * @function checkUserLogin [Check if the user has a valid login to moodle available.]
+ */
 function checkUserLogin(){
   makeXHRreq("https://moodle.plattsburgh.edu/login/index.php", "GET", "text")
-  .then(function(success){
-    console.log(success);
+  .then(function(mainPage){
+    console.log(mainPage);
+    if(mainPage.responseURL.search("moodle.plattsburgh.edu/my") > -1){
+      console.log("User is logged in!");
+    }
+    else if(mainPage.responseURL.search("cas.plattsburgh") > -1){
+      console.log("Send user notification to log in");
+    }
+    else{
+      console.log("Some error occured!");
+    }
   })
   .catch(function(error){
     console.log(error);
