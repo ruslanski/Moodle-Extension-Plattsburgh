cddadf6154cece5c622815ffaaaa98c19487bc1e
Jayant Arora <jaistudy1996@gmail.com>
2017-09-11 23:49:11 -0400

    start parsing events in popup

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,3 +1,4 @@
+/* jshint esversion:6 */
 // JS for message passing
 
 // GLobal Variables
@@ -8,10 +9,52 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     chrome.runtime.sendMessage({request: "EVENTS"}, function(response){
       EVENTS = response.EVENTS;
       console.log(response);
+      addEventsToPopup();
     });
   }
 });
 
+function addEventsToPopup(){
+  for(let i=0; i<1; i++){
+    let form = document.getElementById("todo-list");
+    {
+    	let span_1 = document.createElement('span');
+    	span_1.className = "todo-wrap";
+
+    	let input_1 = document.createElement('input');
+    	input_1.type = "checkbox";
+    	input_1.id = "10";
+    	input_1.checked = false;
+      span_1.appendChild(input_1);
+
+    	let label_1 = document.createElement('label');
+    	label_1.htmlFor = "10";
+    	label_1.className = "todo";
+
+    	let i_1 = document.createElement('i');
+    	i_1.className = "fa fa-check";
+      let text_1 = document.createTextNode("TESTING");
+      label_1.appendChild(i_1);
+      label_1.appendChild(text_1);
+      span_1.appendChild(label_1);
+
+    	let span_2 = document.createElement('span');
+    	span_2.className = "delete-item";
+    	span_2.title = "remove";
+
+    	let i_2 = document.createElement('i');
+    	i_2.className = "fa fa-times-circle";
+      span_2.appendChild(i_2);
+      span_1.appendChild(span_2);
+
+      form.insertBefore(span_1, form.children[form.children.length - 1]);
+    }
+  }
+}
+
+$(document).ready(function(){
+  console.log("ready");
+});
 
 // JS for popup
 
